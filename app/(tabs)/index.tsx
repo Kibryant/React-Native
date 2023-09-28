8e5a3dcaa6177868cf0989771ded363d9a773457
Kibraynt <arthurgustavon@gmail.com>
2023-09-28 10:20:17 -0300

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native'
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import EditScreenInfo from '../../components/EditScreenInfo'
+import { Text, View } from '../../components/Themed'
+import { useState } from 'react'
 
 export default function TabOneScreen() {
+  const [number, setNumber] = useState(0)
   return (
-    <View style={styles.container}>
+    <View className="bg-emerald-500" style={styles.container}>
+      <Text className="text-3xl"> djnfgioadnsfgnoindgiod</Text>
+      <Text> {number} </Text>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <TouchableOpacity
+        onPress={() => setNumber(number + 1)}
+        style={styles.button}
+      >
+        <Text>Click here</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -28,4 +42,9 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-});
+  button: {
+    padding: 10,
+    backgroundColor: '#7c3aed',
+    borderRadius: 8,
+  },
+})
